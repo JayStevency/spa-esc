c995c65168076266b2ba2ce81eb9863f675787f1
jay.yu <jay.yu@jame.me>
2017-11-08 17:57:13 +0900

    product item 정의

--- Crawler/items.py
+++ Crawler/items.py
@@ -6,9 +6,23 @@
 # http://doc.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+from scrapy.loader.processors import TakeFirst
 
 
-class CrawlerItem(scrapy.Item):
+class Product(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+
+    url = scrapy.Field(output_processor=TakeFirst())
+    thumbnail = scrapy.Field(output_processor=TakeFirst())
+    brand = scrapy.Field(output_processor=TakeFirst())
+    # 공백 제거
+    title = scrapy.Field(output_processor=TakeFirst())
+    # , 원 제거
+    price = scrapy.Field(output_processor=TakeFirst())
+    category = scrapy.Field(output_processor=TakeFirst())
+    # 상품번호 : 제거
+    productNo = scrapy.Field(output_processor=TakeFirst())
+    material = scrapy.Field(output_processor=TakeFirst())
+    originalSizeLabel = scrapy.Field(output_processor=str)
+    color = scrapy.Field(output_processor=str)
