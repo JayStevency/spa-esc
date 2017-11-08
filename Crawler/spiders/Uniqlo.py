0d1f43816e4d8bd182e4c394dbe56aabb621e73a
jay.yu <jay.yu@jame.me>
2017-11-08 17:58:19 +0900

    Unqlo spider 프로토 타입 개발

--- Crawler/spiders/Uniqlo.py
+++ Crawler/spiders/Uniqlo.py
@@ -1,12 +1,24 @@
 # -*- coding: utf-8 -*-
 import scrapy
 import re
+from scrapy_splash import SplashRequest
+from scrapy.loader import ItemLoader
+from Crawler.items import Product
 
-from scrapy.spiders import CrawlSpider
 
 class UniqloSpider(scrapy.Spider):
     name = 'Uniqlo'
     start_url = 'http://www.uniqlo.kr/search/searchUniqlo.lecs?pageIndex={0}&query=uniqlo'
+    item_fields = {
+        'title': '//h2[@id="goodsNmArea"]//text()',
+        'productNo': '//li[@class="number"]//text()',
+        'thumbnail': '//*[@id="prodImgDefault"]/a/img/@src',
+        'price': '//*[@id="salePrice"]//text()',
+        'originalSizeLabel': '//*[@id="listChipSize"]/li/a/em/text()',
+        'color': '//*[@id="listChipColor"]/li/a/img/@alt',
+        'material': '//*[@id="prodDetail"]/div/dl[2]/dd/text()',
+        'category': '//*[@id="prodInfo"]/p/a[2]/text()'
+    }
     
     def start_requests(self):
         yield scrapy.Request(url=self.start_url.format(0), callback=self.parse_init)
@@ -19,8 +31,14 @@ class UniqloSpider(scrapy.Spider):
             pass
         else:
             last_index = int(last_index.group()) + 1
+
+        self.parse_list(response=response)
+        # while True:
+        #     yield self.parse_list(response=response)
+        #     break
         
-        for i in range(0, last_index):
+        # for i in range(1, last_index):
+        for i in range(2):
             yield scrapy.Request(url=self.start_url.format(i), callback=self.parse_list)
     
     def parse_list(self, response):
@@ -28,31 +46,15 @@ class UniqloSpider(scrapy.Spider):
         
         for link in links:
             product_no = re.search('\d+', link).group()
-            yield scrapy.Request(url='http://www.uniqlo.kr/display/showDisplayCache.lecs?goodsNo=UQ' + product_no,
-                                 callback=self.parse)
+            yield SplashRequest(url='http://www.uniqlo.kr/display/showDisplayCache.lecs?goodsNo=UQ' + product_no,
+                                callback=self.parse,
+                                args={'wait': 0.5}
+                                )
     
     def parse(self, response):
-        title = response.xpath('//h2[@id="goodsNmArea"]//text()').extract_first()
-        product_no = response.xpath('//li[@class="number"]//text()').extract_first()
-        img = response.xpath('//*[@id="prodImgDefault"]/a/img/@src').extract_first()
-        price = response.xpath('//*[@id="salePrice"]//text()').extract_first()
-        # //*[@id="listChipSize"]/li[2]/a/em
-        size_label = response.xpath('//*[@id="listChipSize"]/li/a/@href').extract()
-        color = response.xpath('//*[@id="listChipColor"]/li/a/img/@src').extract()
-        print(img)
-        # yield {
-        #     'link': response.url
-        # }
-        if title is not None and product_no is not None:
-            title = title.replace('\n', '')
-            title = title.replace('\t', '')
-            title = title.replace('\r', '')
-            product_no = product_no.replace('상품번호 : ', '')
-            yield {
-                'title': title,
-                'product_no': product_no,
-                'img': img,
-                'price': price,
-                'size_label': size_label,
-                'color': color
-            }
+        loader = ItemLoader(item=Product(), response=response)
+        for field, xpath in self.item_fields.items():
+            loader.add_xpath(field, xpath)
+        loader.add_value('url', response.url)
+        loader.add_value('brand', self.name.lower())
+        return loader.load_item()
