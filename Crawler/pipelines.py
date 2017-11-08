00c7a71f5f6898d01f12d8867af5a71d0ab6992e
jay.yu <jay.yu@jame.me>
2017-11-08 17:59:40 +0900

    Product item orm 으로 연결

--- Crawler/pipelines.py
+++ Crawler/pipelines.py
@@ -4,8 +4,30 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+from sqlalchemy.orm import sessionmaker
+from Crawler.model.models import Product, db_connect, create_deals_table
 
 
 class CrawlerPipeline(object):
+    def __init__(self):
+        engine = db_connect()
+        create_deals_table(engine)
+        self.Session = sessionmaker(bind=engine)
+    
     def process_item(self, item, spider):
+        session = self.Session()
+        product = Product(**item)
+        
+        try:
+            session.add(product)
+            session.commit()
+        except:
+            session.rollback()
+            raise
+        finally:
+            session.close()
+        
         return item
+    
+    def close_spider(self, spider):
+        pass
