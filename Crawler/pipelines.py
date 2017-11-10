86af1504bc891680e99480de23fe78d702bc58ca
jay.yu <jay.yu@jame.me>
2017-11-10 14:52:50 +0900

    thumbnail 값 못 받거나 혹은 중복된 데이터 아이템 로드시 디비 적재 안함

--- Crawler/pipelines.py
+++ Crawler/pipelines.py
@@ -5,6 +5,7 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 from sqlalchemy.orm import sessionmaker
+from scrapy.exceptions import DropItem
 from Crawler.model.models import Product, db_connect, create_deals_table
 
 
@@ -13,19 +14,24 @@ class CrawlerPipeline(object):
         engine = db_connect()
         create_deals_table(engine)
         self.Session = sessionmaker(bind=engine)
+        self.productNo_set = set()
     
     def process_item(self, item, spider):
         session = self.Session()
-        product = Product(**item)
-        
-        try:
-            session.add(product)
-            session.commit()
-        except:
-            session.rollback()
-            raise
-        finally:
-            session.close()
+        if item['thumbnail'] is not None and item['productNo'] in self.productNo_set:
+            raise DropItem("Duplicate item found: %s" % item)
+        else:
+            self.productNo_set.add(item['productNo'])
+            product = Product(**item)
+            
+            try:
+                session.add(product)
+                session.commit()
+            except:
+                session.rollback()
+                raise
+            finally:
+                session.close()
         
         return item
     
