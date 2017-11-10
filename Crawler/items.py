9cd80137241629b5d0a26de2b5bcb94e50e7db1a
jay.yu <jay.yu@jame.me>
2017-11-10 14:51:29 +0900

    전처리 모듈 삽입 분리

--- Crawler/items.py
+++ Crawler/items.py
@@ -6,23 +6,19 @@
 # http://doc.scrapy.org/en/latest/topics/items.html
 
 import scrapy
-from scrapy.loader.processors import TakeFirst
 
 
 class Product(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-
-    url = scrapy.Field(output_processor=TakeFirst())
-    thumbnail = scrapy.Field(output_processor=TakeFirst())
-    brand = scrapy.Field(output_processor=TakeFirst())
-    # 공백 제거
-    title = scrapy.Field(output_processor=TakeFirst())
-    # , 원 제거
-    price = scrapy.Field(output_processor=TakeFirst())
-    category = scrapy.Field(output_processor=TakeFirst())
-    # 상품번호 : 제거
-    productNo = scrapy.Field(output_processor=TakeFirst())
-    material = scrapy.Field(output_processor=TakeFirst())
-    originalSizeLabel = scrapy.Field(output_processor=str)
-    color = scrapy.Field(output_processor=str)
+    
+    url = scrapy.Field()
+    thumbnail = scrapy.Field()
+    brand = scrapy.Field()
+    title = scrapy.Field()
+    price = scrapy.Field()
+    category = scrapy.Field()
+    productNo = scrapy.Field()
+    material = scrapy.Field()
+    originalSizeLabel = scrapy.Field()
+    color = scrapy.Field()
