5cb7ea6b1db567aa00bbb7a10743499b00d54c8e
jay.yu <jay.yu@jame.me>
2017-11-10 17:46:33 +0900

    forever21 상품번호를 위한 함수 추가

--- Crawler/util/handle_string.py
+++ Crawler/util/handle_string.py
@@ -21,3 +21,11 @@ class HandleString:
         result = str.strip(data)
         result = re.search('\d+', data).group()
         return result
+    
+    @classmethod
+    def extract_product_id_from_url(cls, data):
+        data = str.strip(data)
+        data = data.lower()
+        result = re.search('(id=|no=)\d+', data).group()
+        result = re.search('\d+', result).group()
+        return result
