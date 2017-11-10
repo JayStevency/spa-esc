e7531650ddea1cc75afbc670c1d244ec81c73cf0
jay.yu <jay.yu@jame.me>
2017-11-10 15:04:43 +0900

    유니클로 크롤러 crawlspider로 변경

--- Crawler/spiders/Uniqlo.py
+++ Crawler/spiders/Uniqlo.py
@@ -1,14 +1,18 @@
 # -*- coding: utf-8 -*-
-import scrapy
-import re
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
 from scrapy_splash import SplashRequest
-from scrapy.loader import ItemLoader
+from Crawler.loders.item_loader import UniqloLoader
 from Crawler.items import Product
 
 
-class UniqloSpider(scrapy.Spider):
-    name = 'Uniqlo'
-    start_url = 'http://www.uniqlo.kr/search/searchUniqlo.lecs?pageIndex={0}&query=uniqlo'
+class UniqloSpider(CrawlSpider):
+    name = 'uniqlo'
+    allowed_domains = ['www.uniqlo.kr']
+    start_urls = [
+        'http://www.uniqlo.kr/display/displayShop.lecs?storeNo=22&siteNo=9&displayNo=UQ1A01A07'
+        '&displayMallNo=UQ1&tracking=header_logo&stonType=P']
+    
     item_fields = {
         'title': '//h2[@id="goodsNmArea"]//text()',
         'productNo': '//li[@class="number"]//text()',
@@ -20,39 +24,24 @@ class UniqloSpider(scrapy.Spider):
         'category': '//*[@id="prodInfo"]/p/a[2]/text()'
     }
     
-    def start_requests(self):
-        yield scrapy.Request(url=self.start_url.format(0), callback=self.parse_init)
+    rules = (
+        Rule(LinkExtractor(allow=('displayNo=UQ*',)), callback='parse_dummy', follow=True),
+    )
     
-    def parse_init(self, response):
-        last_index = response.xpath('//*[@id="endPage"]/a/@href').extract_first()
-        last_index = re.search('\d+', last_index)
-        
-        if last_index is None:
-            pass
-        else:
-            last_index = int(last_index.group()) + 1
+    def parse_dummy(self, response):
+        if 'goodsNo=UQ' in response.url:
+            yield SplashRequest(response.url, callback=self.parse_item, args={'wait': 5})
+    
+    def parse_item(self, response):
+        """ This function parses a sample response. Some contracts are mingled
+            with this docstring.
 
-        self.parse_list(response=response)
-        # while True:
-        #     yield self.parse_list(response=response)
-        #     break
-        
-        # for i in range(1, last_index):
-        for i in range(2):
-            yield scrapy.Request(url=self.start_url.format(i), callback=self.parse_list)
-    
-    def parse_list(self, response):
-        links = response.xpath('//div[@id="BP"]/ul/li/div/div/a/@href').extract()
-        
-        for link in links:
-            product_no = re.search('\d+', link).group()
-            yield SplashRequest(url='http://www.uniqlo.kr/display/showDisplayCache.lecs?goodsNo=UQ' + product_no,
-                                callback=self.parse,
-                                args={'wait': 0.5}
-                                )
-    
-    def parse(self, response):
-        loader = ItemLoader(item=Product(), response=response)
+            @url http://www.uniqlo.kr/display/showDisplayCache.lecs?goodsNo=UQ31088277&displayNo=UQ1A02A01A27&stonType=P&storeNo=22&siteNo=9
+            @returns items 1 16
+            @returns requests 0 0
+            @scrapes  url thumbnail brand title price category productNo material originalSizeLabel color
+            """
+        loader = UniqloLoader(item=Product(), response=response)
         for field, xpath in self.item_fields.items():
             loader.add_xpath(field, xpath)
         loader.add_value('url', response.url)
