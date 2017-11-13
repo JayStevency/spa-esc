94fc09c49c7a9f8125392ab2623701fe130abcd3
jay.yu <jay.yu@jame.me>
2017-11-13 17:26:43 +0900

    pick_tag 모듈 제거

--- Crawler/util/pick_tag.py
+++
@@ -1,7 +0,0 @@
-class Picker:
-    @classmethod
-    def picker_fabric(cls, items):
-        print(items)
-        result = list(filter(lambda x: 'FABRIC' in x, items))[0]
-        result = result.split(':')[1]
-        return str.strip(result)
