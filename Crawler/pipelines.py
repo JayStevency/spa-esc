b9f83c3ab4b3bd6fcf5fa5fe48773e12a256a2e3
jay.yu <jay.yu@jame.me>
2017-11-15 17:02:01 +0900

    filter pipeline으로 분리 및 중복 데이터 update or create

--- Crawler/pipelines.py
+++ Crawler/pipelines.py
@@ -10,28 +10,44 @@ from Crawler.model.models import Product, db_connect, create_deals_table
 from Crawler.util.common import check_essential_element
 
 
+class FilterPipeline(object):
+    def __init__(self):
+        self.item_set = set()
+    
+    def process_item(self, item, spider):
+        check_item = (item.get('brand'), item.get('productNo'))
+        if check_item in self.item_set:
+            raise DropItem("Duplicate item found: %s" % item)
+        else:
+            self.item_set.add(check_item)
+        return item
+
+
 class CrawlerPipeline(object):
     def __init__(self):
         engine = db_connect()
         create_deals_table(engine)
         self.Session = sessionmaker(bind=engine)
-        self.productNo_set = set()
     
     def process_item(self, item, spider):
         session = self.Session()
-        if check_essential_element(item) or item.get('productNo') in self.productNo_set:
+        if check_essential_element(item):
             raise DropItem("Duplicate item found: %s" % item)
         else:
-            self.productNo_set.add(item.get('productNo'))
             product = Product(**item)
             
             try:
-                session.add(product)
-                session.commit()
+                exist_product = session.query(Product).filter_by(productNo=item.get('productNo'),
+                                                                 brand=item.get('brand'))
+                if exist_product.first() is None:
+                    session.add(product)
+                else:
+                    exist_product.update(item)
             except:
                 session.rollback()
                 raise
             finally:
+                session.commit()
                 session.close()
         
         return item
