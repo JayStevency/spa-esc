547b224d9b0b67a9f2c35ce7905e107ce935722f
jay.yu <jay.yu@jame.me>
2017-11-21 15:27:31 +0900

    essential check categroy 추가

--- Crawler/util/common.py
+++ Crawler/util/common.py
@@ -1,5 +1,5 @@
 def check_essential_element(items):
-    essential_keys = ['url', 'thumbnail', 'brand', 'title', 'price', 'originalSizeLabel']
+    essential_keys = ['url', 'thumbnail', 'brand', 'title', 'price', 'originalSizeLabel', 'category']
     for key in essential_keys:
         if items.get(key) is None:
             return True
