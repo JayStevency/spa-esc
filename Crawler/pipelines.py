caf91ec0335ccbc4c764f7889c6f51f62efb0b13
jay.yu <jay.yu@jame.me>
2017-11-21 15:28:01 +0900

    Category 파이프 라인 추가

--- Crawler/pipelines.py
+++ Crawler/pipelines.py
@@ -4,10 +4,22 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import logging
 from sqlalchemy.orm import sessionmaker
 from scrapy.exceptions import DropItem
+from scrapy.utils.log import configure_logging
 from Crawler.model.models import Product, db_connect, create_deals_table
 from Crawler.util.common import check_essential_element
+from Crawler.util.category.category_processing import Categorizing
+
+logger = logging.getLogger('scrapy_logger')
+
+
+class CategoryPipeline(object):
+    def process_item(self, item, spider):
+        category = Categorizing(item=item)
+        category.convert_category()
+        return category.get_item()
 
 
 class FilterPipeline(object):
@@ -32,22 +44,29 @@ class CrawlerPipeline(object):
     def process_item(self, item, spider):
         session = self.Session()
         if check_essential_element(item):
+            configure_logging(install_root_handler=False)
+            logging.basicConfig(
+                filename='log.txt',
+                format='%(levelname)s: %(message)s',
+                level=logging.INFO
+            )
+            logging.info(item)
             raise DropItem("Duplicate item found: %s" % item)
         else:
             product = Product(**item)
             
             try:
-                exist_product = session.query(Product).filter_by(productNo=item.get('productNo'),
-                                                                 brand=item.get('brand'))
-                if exist_product.first() is None:
+                if session.query(Product).filter_by(productNo=item.get('productNo'),
+                                                    brand=item.get('brand')).first() is None:
                     session.add(product)
                 else:
-                    exist_product.update(item)
+                    session.query(Product).filter_by(productNo=item.get('productNo'), brand=item.get('brand')).update(
+                        item)
+                session.commit()
             except:
                 session.rollback()
                 raise
             finally:
-                session.commit()
                 session.close()
         
         return item
