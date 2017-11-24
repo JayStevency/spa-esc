71761eb5a75fd3f07e09108a1c4d2d3950cbf6bd
jay <jay@jaemui-MacBook-Pro-2.local>
2017-11-24 18:19:51 +0900

    Remove comments

--- Crawler/items.py
+++ Crawler/items.py
@@ -1,17 +1,7 @@
-# -*- coding: utf-8 -*-
-
-# Define here the models for your scraped items
-#
-# See documentation in:
-# http://doc.scrapy.org/en/latest/topics/items.html
-
 import scrapy
 
 
 class Product(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    
     url = scrapy.Field()
     thumbnail = scrapy.Field()
     brand = scrapy.Field()
@@ -24,4 +14,3 @@ class Product(scrapy.Item):
     material = scrapy.Field()
     originalSizeLabel = scrapy.Field()
     color = scrapy.Field()
-    
