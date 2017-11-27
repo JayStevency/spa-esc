d0b11b572fd6c585289a9f2072ceaa241f8c4f51
jay <jay@jaemui-MacBook-Pro-2.local>
2017-11-27 16:07:15 +0900

    Cleanse original category

--- Crawler/loaders.py
+++ Crawler/loaders.py
@@ -2,6 +2,7 @@ from scrapy.loader import ItemLoader
 from scrapy.loader.processors import TakeFirst, MapCompose, Identity
 from Crawler.util.handle_string import HandleString
 from w3lib.html import replace_entities, replace_escape_chars
+from Crawler.util.common import replace_useless_chars
 
 
 class UniqloLoader(ItemLoader):
@@ -33,10 +34,11 @@ class Forever21Loader(ItemLoader):
 
 class BalaanLoader(ItemLoader):
     default_output_processor = TakeFirst()
-
+    
     price_in = MapCompose(HandleString.extract_digit_from_price)
     title_in = MapCompose(replace_entities, replace_escape_chars)
     brand_in = MapCompose(replace_entities, replace_escape_chars)
+    originalCategory_in = MapCompose(Identity, replace_useless_chars)
 
     originalCategory_out = str
     category_out = str
