bf170d8ff2e7a3ca4e8584dc93615c061d3e841f
jay <jay@jaemui-MacBook-Pro-2.local>
2017-11-27 15:38:03 +0900

    Add balaan loader

--- Crawler/loaders.py
+++ Crawler/loaders.py
@@ -1,6 +1,7 @@
 from scrapy.loader import ItemLoader
-from scrapy.loader.processors import TakeFirst, MapCompose, Join
+from scrapy.loader.processors import TakeFirst, MapCompose, Identity
 from Crawler.util.handle_string import HandleString
+from w3lib.html import replace_entities, replace_escape_chars
 
 
 class UniqloLoader(ItemLoader):
@@ -24,7 +25,18 @@ class Forever21Loader(ItemLoader):
     salePrice_in = MapCompose(HandleString.extract_digit_from_price)
     productNo_in = MapCompose(HandleString.extract_product_id_from_url)
     material_in = MapCompose(HandleString.remove_whitespace)
-
+    
     originalCategory_out = str
     originalSizeLabel_out = str
     color_out = str
+
+
+class BalaanLoader(ItemLoader):
+    default_output_processor = TakeFirst()
+    
+    title_in = MapCompose(replace_entities, replace_escape_chars)
+    brand_in = MapCompose(replace_entities, replace_escape_chars)
+    
+    category_out = Identity()
+    thumbnail_out = Identity()
+    originalSizeLabel_out = Identity()
