00338b82ac7ee121d6f9d94c6a6cb10b994e0dfd
jay <jay@jaemui-MacBook-Pro-2.local>
2017-11-27 16:27:34 +0900

    Add taking unique in list func

--- Crawler/util/common.py
+++ Crawler/util/common.py
@@ -11,3 +11,10 @@ def replace_useless_chars(text, which_ones=('', '\xa0'), replace_by=''):
     for uc in which_ones:
         text = text.replace(uc, replace_by)
     return text
+
+
+class TakeUnique(object):
+    def __call__(self, values):
+        if '' in values:
+            values.remove('')
+        return values
