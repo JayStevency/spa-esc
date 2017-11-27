bdadef8c5bf5ea0d819af9b052b53e16f3122800
jay <jay@jaemui-MacBook-Pro-2.local>
2017-11-27 16:02:16 +0900

    Add empty str removing func

--- Crawler/util/common.py
+++ Crawler/util/common.py
@@ -5,3 +5,9 @@ def check_essential_element(items):
             return True
     
     return False
+
+
+def replace_useless_chars(text, which_ones=('', '\xa0'), replace_by=''):
+    for uc in which_ones:
+        text = text.replace(uc, replace_by)
+    return text
