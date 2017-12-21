ebe756e3a3bd89f88a946e5168d6c31c1f6cde55
jay.yu <jay.yu@jame.me>
2017-12-21 17:46:43 +0900

    cpu 부하를 줄이기 위한 세팅

--- Crawler/settings.py
+++ Crawler/settings.py
@@ -21,14 +21,13 @@ USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36
 ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-CONCURRENT_REQUESTS = 16
 
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
 # Configure concurrent requests performed by Scrapy
 DOWNLOAD_DELAY = 0.05
-CONCURRENT_REQUESTS = 24
+CONCURRENT_REQUESTS = 12
 CONCURRENT_REQUESTS_PER_DOMAIN = 12
 CONCURRENT_REQUESTS_PER_IP = 12
 
@@ -41,7 +40,7 @@ AUTOTHROTTLE_TARGET_CONCURRENCY = 3
 
 
 # Disable cookies (enabled by default)
-# COOKIES_ENABLED = False
+COOKIES_ENABLED = False
 
 # Disable Telnet Console (enabled by default)
 # TELNETCONSOLE_ENABLED = False
@@ -76,22 +75,10 @@ ITEM_PIPELINES = {
    'Crawler.pipelines.FilterPipeline': 100,
    'Crawler.pipelines.CategoryPipeline': 200,
    'Crawler.pipelines.CrawlerPipeline': 300,
-   'Crawler.pipelines.ESPipeline': 400,
+   # 'Crawler.pipelines.ESPipeline': 400,
    
 }
-
-# Enable and configure the AutoThrottle extension (disabled by default)
-# See http://doc.scrapy.org/en/latest/topics/autothrottle.html
-# AUTOTHROTTLE_ENABLED = True
-# The initial download delay
-# AUTOTHROTTLE_START_DELAY = 5
-# The maximum download delay to be set in case of high latencies
-# AUTOTHROTTLE_MAX_DELAY = 60
-# The average number of requests Scrapy should be sending in parallel to
-# each remote server
-# AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
-# Enable showing throttling stats for every response received:
-# AUTOTHROTTLE_DEBUG = False
+REACTOR_THREADPOOL_MAXSIZE = 20
 
 # Enable and configure HTTP caching (disabled by default)
 # See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
@@ -100,6 +87,8 @@ ITEM_PIPELINES = {
 # HTTPCACHE_DIR = 'httpcache'
 # HTTPCACHE_IGNORE_HTTP_CODES = []
 # HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+
+LOG_LEVEL = 'INFO'
 
 FEED_EXPORT_ENCODING = 'utf-8'
 FEED_EXPORT_FIELDS = ['url', 'thumbnail', 'brand', 'title', 'price', 'originalSizeLabel', 'category']
