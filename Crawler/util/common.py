7551404bbc5c4e14b4c43275fdbdc30a954ba2e6
jay.yu <jay.yu@jame.me>
2017-12-26 11:43:26 +0900

    replace_useless_chars 함수 추가

--- Crawler/util/common.py
+++ Crawler/util/common.py
@@ -13,6 +13,12 @@ def replace_useless_chars(text, which_ones=('', '\xa0'), replace_by=''):
     return text
 
 
+def replace_useless_chars(text, which_ones=('\xa0', '\t', '\r', '\n', '&nbsp'), replace_by=''):
+    for uc in which_ones:
+        text = text.replace(uc, replace_by)
+    return text
+
+
 class TakeUnique(object):
     def __call__(self, values):
         if '' in values:
