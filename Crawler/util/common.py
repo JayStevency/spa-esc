1ea8b39aee07fd43aa34ecfe1b41d00a1c5eae38
jay.yu <jay.yu@jame.me>
2017-12-27 16:37:20 +0900

    중복 함수 제거

--- Crawler/util/common.py
+++ Crawler/util/common.py
@@ -1,16 +1,11 @@
 def check_essential_element(items):
-    essential_keys = ['url', 'thumbnail', 'brand', 'title', 'price', 'originalSizeLabel', 'category']
+    # essential_keys = ['url', 'thumbnail', 'brand', 'title', 'price', 'originalSizeLabel', 'category']
+    essential_keys = ['url', 'thumbnail', 'brand', 'title', 'price', ]
     for key in essential_keys:
         if items.get(key) is None:
             return True
     
     return False
-
-
-def replace_useless_chars(text, which_ones=('', '\xa0'), replace_by=''):
-    for uc in which_ones:
-        text = text.replace(uc, replace_by)
-    return text
 
 
 def replace_useless_chars(text, which_ones=('\xa0', '\t', '\r', '\n', '&nbsp'), replace_by=''):
